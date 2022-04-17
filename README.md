e9d2c7416c6ef06cf3abab0557bd358f77bbd453
ekatynski <edkatynski@gmail.com>
2022-04-17 02:13:19 -0400

    Updated README, made ping requests asynchronous.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pinger
+
+An attempt to automate a data-collection function for a networking class.
--- pinger.sh
+++ pinger.sh
@@ -1,6 +1,7 @@
 mkdir -p output
-ping -w60 google.com >> output/google_ping.txt
-ping -w60 stackoverflow.com >> output/so_ping.txt
-ping -w60 npr.org >> output/npr_ping.txt
-ping -w60 nasa.gov >> output/nasa_ping.txt
-ping -w60 umich.edu >> output/umich_ping.txt
+ping -w60 google.com >> output/google_ping.txt &
+ping -w60 stackoverflow.com >> output/so_ping.txt &
+ping -w60 npr.org >> output/npr_ping.txt &
+ping -w60 nasa.gov >> output/nasa_ping.txt &
+ping -w60 umich.edu >> output/umich_ping.txt &
+

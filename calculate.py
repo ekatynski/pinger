07e32cb0b9902db123579522e92cdbe811333e1e
ekatynski <edkatynski@gmail.com>
2022-04-21 14:48:23 -0400

    Restructured entire file-reading approach to simplify entry to dataframe.

--- calculate.py
+++ calculate.py
@@ -1,79 +1,62 @@
+from ast import Interactive
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib import interactive
 from dateutil import parser
 import dir_setup
 
 def read_file(filename):
     filename = 'output/' + filename
-    result = {'requests': 0, 'min': float('inf'), 'max': 0, 'avg': 0, 'data': []}
+    rtt_times = []
+    rtt_stamps = []
+    rtt_count = 0
 
     with open(filename, 'r') as f:
             line = f.readline()
             # before the file ends
             while line:
-                # if you reach the ping statistics header
-                if 'statistics' in line:
-                    result['requests'] += 1
-
-                    # extract packet loss percentage
+                if 'time=' in line:
+                    # extract RTT time from line
+                    rtt_count += 1
+                    rtt_times.append(float((line.split('=')[3])[:4])) 
                     line = f.readline()
-                    pl_percent = float(line.split(',')[2].split('%')[0])
 
-                    # extract rtt statistics
-                    line = f.readline()
-                    rtt_vals = line.split('=')[1].split('/')[0:3]
-                    rtt_stats = {'min':float(rtt_vals[0]), 'avg':float(rtt_vals[1]), 'max':float(rtt_vals[2])}
-
+                elif 'DATE:' in line:
                     # extract the timestamp
-                    line = f.readline()
                     date = parser.parse(line[11:len(line) - 5])
-
-                    # process data for return
-                    result['data'].append({'packet_loss': pl_percent, 'stats': rtt_stats, 'date': date})
-                    result['avg'] += rtt_stats['avg']
-                    if rtt_stats['min'] < result['min']: result['min'] = rtt_stats['min']
-                    if rtt_stats['max'] > result['max']: result['max'] = rtt_stats['max']
+                    rtt_stamps = rtt_stamps + ([date] * rtt_count)
 
                     # advance to the next line
+                    rtt_count = 0
                     line = f.readline()
 
                 else:
                     # skip the line
                     line = f.readline()
 
-    # calculate average rtt
-    result['avg'] = round(result['avg'] / result['requests'], 3)
-    return result
-
-def plot(ping_frame):
-    for category in ['min', 'max', 'avg']:
-        ping_frame.plot(x='timestamp', y=category)
-        
+    # produce dataframe to return
+    data = {'rtt': rtt_times, 'timestamp': rtt_stamps}
+    df = pd.DataFrame(data)
+    return df
 
 if __name__ == "__main__":
     dir_setup.dir_setup("output/results")
     directory = os.fsencode('output')
     
-    ping_results = {}
-    frame_results = pd.DataFrame(columns=['name', 'min', 'max', 'avg', 'timestamp'])
+    frame_results = {}
+    website_names = []
 
     # iterate through all output ping data files for analysis
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
         if filename.endswith('.txt'):
-            website_name = filename.split('.')[0]
+            website_names.append(filename.split('_')[0])
 
             # read from the file and extract the website, ping timestamps, loss rate, and RTT stats into a dict per site
-            ping_results[website_name] = read_file(filename)
+            frame_results[filename.split('.')[0]] = read_file(filename)
         
             # insert website data into dataframe
-            for item in ping_results[website_name]['data']:
-                new_row = {'name': website_name.split('_')[0], 'min': item['stats']['min'], 'max': item['stats']['max'], 'avg': item['stats']['avg'], 'timestamp': item['date']}
-                frame_results = frame_results.append(new_row, ignore_index=True)
     
-    frame_results.plot(x='timestamp', y='avg')
-    plt.show()
-
     print(frame_results)
-    
+    print("\nDone!")
